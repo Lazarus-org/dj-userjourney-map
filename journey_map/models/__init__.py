2ca87af43ebabb58f208e7ebcb261d45cca4b821
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2025-04-14 16:48:45 +0330

    refactor(models): Add pain point & opportynity to main models

--- journey_map/models/__init__.py
+++ journey_map/models/__init__.py
@@ -2,3 +2,5 @@ from .user_journey import UserJourney
 from .journey_action import JourneyAction
 from .journey_stage import JourneyStage
 from .user_feedback import UserFeedback
+from .pain_point import PainPoint
+from .opportunity import Opportunity
