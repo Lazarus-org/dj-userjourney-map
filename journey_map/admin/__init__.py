18de9037b97451e1c21b8f6f06740b578c8cf547
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2025-04-17 17:27:58 +0330

    feat(admin): Add all model admins with dynamic site support
    Closes #20
    Closes #30

---
+++ journey_map/admin/__init__.py
@@ -0,0 +1,6 @@
+from .user_journey import UserJourneyAdmin
+from .journey_stage import JourneyStageAdmin
+from .journey_action import JourneyActionAdmin
+from .user_feedback import UserFeedbackAdmin
+from .pain_point import PainPointAdmin
+from .opportunity import OpportunityAdmin
